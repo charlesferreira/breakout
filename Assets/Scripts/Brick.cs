ed24edf810acc65df87a1236adcf85b2ee7ffc50
Charles Ferreira <charles.dev@live.com>
2021-02-10 22:11:18 -0300

    Ajeita a interface do jogo

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -2,8 +2,19 @@ using UnityEngine;
 
 public class Brick : MonoBehaviour
 {
-    [SerializeField] int hits = 1;
-    [SerializeField] int points = 100;
+    [SerializeField] private int hits = 1;
+    [SerializeField] private int points = 100;
+
+    [SerializeField] private Material hitMaterial;
+
+    new private Renderer renderer;
+    private Material originalMaterial;
+
+    private void Awake()
+    {
+        renderer = GetComponent<Renderer>();
+        originalMaterial = renderer.sharedMaterial;
+    }
 
     private void OnCollisionEnter(Collision other)
     {
@@ -12,5 +23,15 @@ public class Brick : MonoBehaviour
         {
             Destroy(gameObject);
         }
+        else
+        {
+            renderer.material = hitMaterial;
+            Invoke("ResetMaterial", .1f);
+        }
+    }
+
+    private void ResetMaterial()
+    {
+        renderer.material = originalMaterial;
     }
 }
