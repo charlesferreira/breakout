72651d4b7ce4828f96556d4fa22523c25bde1da7
Charles Ferreira <charles.dev@live.com>
2021-03-03 11:05:34 -0300

    Cria scripts para variáveis como assets

---
+++ Assets/Scripts/FloatVariable.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FloatVariable", menuName = "Breakout/Variable/Float", order = 0)]
+public class FloatVariable : ScriptableObject
+{
+    public float value;
+}
---
+++ Assets/Scripts/IntVariable.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "IntVariable", menuName = "Breakout/Variable/Integer", order = 0)]
+public class IntVariable : ScriptableObject
+{
+    public int value;
+}
